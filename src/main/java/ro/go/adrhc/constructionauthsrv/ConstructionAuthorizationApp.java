d8bd4a70232321bced462ef842892ede937b594a
Adrian <adrhc@outlook.com>
2024-03-28 16:13:09 +0200

    ConstructionAuthorizationApp

--- src/main/java/ro/go/adrhc/constructionauthsrv/ConstructionAuthorizationApp.java
+++ src/main/java/ro/go/adrhc/constructionauthsrv/ConstructionAuthorizationApp.java
@@ -3,9 +3,6 @@ package ro.go.adrhc.constructionauthsrv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- */
 @SpringBootApplication
 public class ConstructionAuthorizationApp {
     public static void main(String[] args) {
