9e4e5659868f1054164d013b51888974fbfd6d5a
Adrian <adrhc@outlook.com>
2024-10-23 19:47:07 +0300

    ConstructionAuthorizationAppTest: index.readonly=true

--- src/test/java/ro/go/adrhc/constructionauthsrv/ConstructionAuthorizationAppTest.java
+++ src/test/java/ro/go/adrhc/constructionauthsrv/ConstructionAuthorizationAppTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "index.readonly=true")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ConstructionAuthorizationAppTest {
     @Test
